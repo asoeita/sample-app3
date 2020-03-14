e5f226d6e0ab4479032b5fa37dd6a244a84d00c7
Eita Aso <muscle03s@gmail.com>
2020-03-14 15:16:57 +0900

    finish ch09

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,8 @@ class SessionsController < ApplicationController
     # => User object or false
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # remember user #save to DB + cookies[:token] #SessionsHelper
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
@@ -18,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
